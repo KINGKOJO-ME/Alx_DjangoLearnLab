9596f5dc2666ae43621fdd9a7fb64fdd2b61feda
Kojo <ampah.kojo55@gmail.com>
2026-02-11 23:58:48 +0000

    changes made to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -44,3 +44,25 @@ if __name__ == "__main__":
     books_by_author("George Orwell")
     books_in_library("Central Library")
     librarian_of_library("Central Library")
+
+#Query 1: All books by a specific author
+def books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        print(f"Books by {author_name}:")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print(f"No author found with name {author_name}")
+
+#retrieving the librarian for a library
+def librarian_of_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        print(f"Librarian of {library_name}: {librarian.name}")
+    except Library.DoesNotExist:
+        print(f"No library found with name {library_name}")
+    except Librarian.DoesNotExist:
+        print(f"No librarian assigned to {library_name}")
