5c3ec1ff17afeceba0288d33aaa41a926296e464
Kojo <ampah.kojo55@gmail.com>
2026-02-13 14:47:18 +0000

    fixing issues with views setting urls 01

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
-]
-
-# Add login view for user authentication]
-
-from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register
-
-urlpatterns = [
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
     path("register/", register, name="register"),
 ]
-
