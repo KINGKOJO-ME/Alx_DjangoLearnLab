f50f423bb40d809fb6312d459cadebe09d922cd8
Kojo <ampah.kojo55@gmail.com>
2026-02-13 16:36:05 +0000

    urls path fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -30,6 +30,11 @@ urlpatterns = [
 
 # Include book management views (protected by permissions)
 
-path("add-book/", views.add_book, name="add_book"),
-path("edit-book/", views.edit_book, name="edit_book"),
-path("delete-book/", views.delete_book, name="delete_book"),
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+]
