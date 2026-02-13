689901d6158d9a9717f0b0a2c27e04fe2fb2d5e9
Kojo <ampah.kojo55@gmail.com>
2026-02-13 15:43:17 +0000

    Add URL routes for role-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,3 +15,14 @@ urlpatterns = [
         name='logout'
     ),
 ]
+
+# Include role-based views
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+]
