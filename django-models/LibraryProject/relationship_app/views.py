2752211c14369d1c50f7baaad464616ff45bf20d
Kojo <ampah.kojo55@gmail.com>
2026-02-13 16:17:44 +0000

    Secure book views using permission_required decorator

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -88,3 +88,23 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+
+# Sample view with permission check
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+
+@permission_required("relationship_app.can_add_book", raise_exception=True)
+def add_book(request):
+    return render(request, "relationship_app/add_book.html")
+
+
+@permission_required("relationship_app.can_change_book", raise_exception=True)
+def edit_book(request):
+    return render(request, "relationship_app/edit_book.html")
+
+
+@permission_required("relationship_app.can_delete_book", raise_exception=True)
+def delete_book(request):
+    return render(request, "relationship_app/delete_book.html")
