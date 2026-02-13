3ffe852998a41dd4ddc78e121bb32250ae02a8a0
Kojo <ampah.kojo55@gmail.com>
2026-02-13 10:54:50 +0000

    Configured URLs for Function-based book List view

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import list_books
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def list_books(request):
+    #Function-based view that retrieves all books and displays them in a template.
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books
+    }
+
+    return render(request, 'relationship_app/list_books.html', context)
