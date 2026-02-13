5d24af9a5232de764f17bca45440894b9c9e0c1f
Kojo <ampah.kojo55@gmail.com>
2026-02-13 11:07:31 +0000

    Implemented class-based library detail view and configured URLs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,3 +12,17 @@ def list_books(request):
     }
 
     return render(request, 'relationship_app/list_books.html', context)
+
+from django.views.generic import DetailView
+from .models import Library
+
+
+class LibraryDetailView(DetailView):
+    """
+    Class-based view to display details of a specific library
+    and list all books available in that library.
+    """
+
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
