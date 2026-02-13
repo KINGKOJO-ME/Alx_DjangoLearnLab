d56e6c8cd682acb8115d858a731f9707ebdaba8f
Kojo <ampah.kojo55@gmail.com>
2026-02-13 16:19:55 +0000

    Add URL routes for permission-protected book actions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -26,3 +26,10 @@ urlpatterns = [
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
 ]
+
+
+# Include book management views (protected by permissions)
+
+path("add-book/", views.add_book, name="add_book"),
+path("edit-book/", views.edit_book, name="edit_book"),
+path("delete-book/", views.delete_book, name="delete_book"),
