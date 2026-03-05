f2e0938d75d618f27675d3b9ba4b0414112899a7
Kojo <ampah.kojo55@gmail.com>
2026-03-05 08:59:09 +0000

    forms fix

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,21 +1,24 @@
 from django import forms
 from .models import Book
 
+
+class ExampleForm(forms.Form):
+    """
+    Example form used to demonstrate secure handling of user input.
+    Django automatically validates and sanitizes the input fields.
+    """
+
+    title = forms.CharField(max_length=200)
+    author = forms.CharField(max_length=100)
+    publication_year = forms.IntegerField()
+
+
 class BookForm(forms.ModelForm):
     """
-    Secure form for creating and updating Book objects.
-    Django forms automatically validate and sanitize input,
-    helping prevent SQL injection and other malicious input.
+    ModelForm connected to the Book model.
+    It allows users to safely create Book objects using Django's ORM.
     """
 
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
-
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-
-        if len(title) < 2:
-            raise forms.ValidationError("Title must contain at least 2 characters.")
-
-        return title
