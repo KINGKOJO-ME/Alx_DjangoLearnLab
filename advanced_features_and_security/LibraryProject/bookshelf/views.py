225f2f22c60ab05bdfb4376c0af2af39fce628a8
Kojo <ampah.kojo55@gmail.com>
2026-03-05 08:50:43 +0000

    forms fix

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Book
 from .forms import BookForm
 
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
 def add_book(request):
-    """
-    View to securely create a new book using Django forms.
-    """
 
     if request.method == "POST":
         form = BookForm(request.POST)
