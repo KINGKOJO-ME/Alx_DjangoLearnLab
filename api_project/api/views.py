29dd08b1c0b66ccfba11a35ea3e9eb623fb94ae8
Kojo <ampah.kojo55@gmail.com>
2026-03-08 01:12:03 +0000

    Implemented BookList API endpoint using Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+# API view to list all books
+# Uses DRF's ListAPIView to return a queryset of Book objects
+class BookList(generics.ListAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
