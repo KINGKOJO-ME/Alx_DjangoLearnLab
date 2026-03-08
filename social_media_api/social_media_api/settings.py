ac8ed603861b7a0778f5912999dba3d9d43037d9
Kojo <ampah.kojo55@gmail.com>
2026-03-08 16:35:35 +0000

    Implemented posts and comments API with CRUD operations, filtering and pagination

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     'accounts',
+
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,17 @@ REST_FRAMEWORK = {
     ]
 }
 
+
+
+# add pagination settings
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 10
+}
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+
     path('api/accounts/', include('accounts.urls')),
+
+    path('api/', include('posts.urls')),
 ]
