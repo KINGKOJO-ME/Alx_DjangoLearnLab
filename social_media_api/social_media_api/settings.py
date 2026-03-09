2cbbede4caa2f414a9d7dce54b0254867bc705e8
Kojo <ampah.kojo55@gmail.com>
2026-03-09 09:50:48 +0000

    Added PORT configuration to database settings for deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
