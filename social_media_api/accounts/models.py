24a99727d58e62fa4b2985c5699f443b3a1cddd0
Kojo <ampah.kojo55@gmail.com>
2026-03-09 08:07:56 +0000

    Implemented follow/unfollow system and feed endpoint

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -68,3 +68,28 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author}"
+    
+
+    # followers and following relationships
+    # are handled in the User model using a ManyToManyField
+
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    bio = models.TextField(blank=True)
+
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -13,3 +13,36 @@ class LoginView(ObtainAuthToken):
     pass
 
 # Ensure you also have a ProfileView if your URLs require it
+
+
+
+
+# follow and unfollow views
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+
+    user_to_follow = User.objects.get(id=user_id)
+
+    request.user.following.add(user_to_follow)
+
+    return Response({"message": f"You are now following {user_to_follow.username}"})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+
+    user_to_unfollow = User.objects.get(id=user_id)
+
+    request.user.following.remove(user_to_unfollow)
+
+    return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
