75fc993a117dd84423c7a058a21abde0be7bf2c9
Kojo <ampah.kojo55@gmail.com>
2026-03-09 08:14:58 +0000

    Implemented follow and unfollow views using GenericAPIView with authentication permissions

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -18,31 +18,45 @@ class LoginView(ObtainAuthToken):
 
 
 # follow and unfollow views
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
 
-User = get_user_model()
-
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def follow_user(request, user_id):
-
-    user_to_follow = User.objects.get(id=user_id)
-
-    request.user.following.add(user_to_follow)
-
-    return Response({"message": f"You are now following {user_to_follow.username}"})
+CustomUser = get_user_model()
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def unfollow_user(request, user_id):
+class FollowUserView(generics.GenericAPIView):
 
-    user_to_unfollow = User.objects.get(id=user_id)
+    permission_classes = [permissions.IsAuthenticated]
 
-    request.user.following.remove(user_to_unfollow)
+    queryset = CustomUser.objects.all()
 
-    return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
+    def post(self, request, user_id):
+
+        user_to_follow = CustomUser.objects.get(id=user_id)
+
+        request.user.following.add(user_to_follow)
+
+        return Response(
+            {"message": f"You are now following {user_to_follow.username}"},
+            status=status.HTTP_200_OK
+        )
+
+
+class UnfollowUserView(generics.GenericAPIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = CustomUser.objects.all()
+
+    def post(self, request, user_id):
+
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+
+        return Response(
+            {"message": f"You unfollowed {user_to_unfollow.username}"},
+            status=status.HTTP_200_OK
+        )
